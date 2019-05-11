101d6ae8508f7794c9920fce498cd3008f4b9fa5
wangjunjie0716 <junjie90716@qq.com>
2019-05-11 21:16:16 +0800

    message

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     url(r'^', include('apps.users.urls', namespace='users')),
+    #url(r'^', include('apps.users.urls', namespace='register')),
 ]
